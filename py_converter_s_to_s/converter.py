67cdab890960e068305e9df5acf843eff00c1331
Bolol <43433312+BololZ@users.noreply.github.com>
2025-03-10 15:00:43 +0100

    FEAT - more class style ??  and bump to 0.6.1

--- py_converter_s_to_s/converter.py
+++ py_converter_s_to_s/converter.py
@@ -12,64 +12,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-CURRENT_FILE_PATH = ""
-FILE_NAME_LABEL = None
-CANVAS = None
-FIG = None
-
-def audio_to_spectrogram(file_path, cmap):
-    """Convertit un fichier audio en spectrogramme."""
-    # Charger le fichier audio
-    y, sr = librosa.load(file_path)
-
-    # Calculer le spectrogramme
-    d = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
-
-    # Caclul taille du spectrogramme
-    canvas_width, canvas_height = CANVAS.get_width_height()
-
-    # Afficher le spectrogramme
-    plt.figure(figsize=(canvas_height/100, canvas_width/100))
-    librosa.display.specshow(d, sr=sr, x_axis='time', y_axis='log', cmap=cmap)
-    plt.colorbar(format='%+2.0f dB')
-    plt.title('Spectrogramme')
-    plt.tight_layout()
-
-    return plt.gcf()
-
-def load_audio_file():
-    """Charge un fichier audio et met à jour le spectrogramme."""
-    file_path, _ = QFileDialog.getOpenFileName(None, "Open Audio File", "",
-                                               "Audio Files (*.wav *.mp3 *.flac)")
-    if file_path:
-        global CURRENT_FILE_PATH
-        CURRENT_FILE_PATH = file_path
-        FILE_NAME_LABEL.setText(f"Fichier chargé : {file_path.split('/')[-1]}")
-        update_spectrogram(cmap='viridis')
-
-def update_spectrogram(cmap='viridis'):
-    """Met à jour le spectrogramme avec les paramètres actuels."""
-    if globals().get('CURRENT_FILE_PATH'):
-        global FIG
-        FIG = audio_to_spectrogram(CURRENT_FILE_PATH, cmap)
-        CANVAS.figure = FIG
-        CANVAS.draw()
-
-def save_spectrogram():
-    """Enregistre le spectrogramme dans un fichier PNG."""
-    file_path, _ = QFileDialog.getSaveFileName(None, "Save Spectrogram", "", "PNG files (*.png)")
-    if file_path:
-        CANVAS.figure.saveFIG(file_path)
-
-def on_closing():
-    """Fonction appelée lorsque la fenêtre est fermée."""
-    QApplication.quit()
 
 class MainWindow(QMainWindow):
     """Classe principale pour la fenêtre de l'application."""
     def __init__(self):
         super().__init__()
+        self.current_file_path = None
+        self.file_name_label = QLabel("Aucun fichier chargé")
+        self.canvas = None
+        self.fig = None
+        self.cmap = plt.get_cmap('viridis')
 
+        # Set window title and size
         self.setWindowTitle("Spectrogramme Audio")
         # Get screen dimensions
         screen_width = self.screen().size().width()
@@ -82,50 +36,101 @@ class MainWindow(QMainWindow):
 
         self.init_ui()
 
+    def audio_to_spectrogram(self):
+        """Convertit un fichier audio en spectrogramme."""
+        if self.current_file_path:
+            # Charger le fichier audio avec librosa
+            y, sr = librosa.load(self.current_file_path)
+
+            # Calculer le spectrogramme
+            d = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+
+            # Caclul taille du spectrogramme
+            canvas_width, canvas_height = self.canvas.get_width_height()
+
+            # Afficher le spectrogramme
+            plt.figure(figsize=(canvas_height/100, canvas_width/100))
+            librosa.display.specshow(d, sr=sr, x_axis='time', y_axis='log', cmap=self.cmap)
+            plt.colorbar(format='%+2.0f dB')
+            plt.title('Spectrogramme')
+            plt.tight_layout()
+
+            return plt.gcf()
+
+        return None
+
+    def load_audio_file(self):
+        """Charge un fichier audio et met à jour le spectrogramme."""
+        self.current_file_path, _ = QFileDialog.getOpenFileName(None, "Open Audio File", "",
+                                                "Audio Files (*.wav *.mp3 *.flac)")
+        if self.current_file_path:
+            self.file_name_label.setText(f"Fichier chargé : "
+                                         f"{self.current_file_path.split('/')[-1]}")
+            self.update_spectrogram()
+
+    def update_spectrogram(self):
+        """Met à jour le spectrogramme avec les paramètres actuels."""
+        self.fig = self.audio_to_spectrogram()
+        if self.fig:
+            self.canvas.figure = self.fig
+            self.canvas.draw()
+
+    def save_spectrogram(self):
+        """Enregistre le spectrogramme dans un fichier PNG."""
+        file_path, _ = QFileDialog.getSaveFileName(None, "Save Spectrogram", "",
+                                                    "PNG files (*.png)")
+        if file_path:
+            self.canvas.figure.savefig(file_path)
+
+    def on_closing(self):
+        """Fonction appelée lorsque la fenêtre est fermée."""
+        QApplication.quit()
+
     def init_ui(self):
         """Initialise l'interface utilisateur."""
-        global FILE_NAME_LABEL, CANVAS, FIG
-
         # Create the main widget and layout
-        main_widget = QWidget()
-        layout = QVBoxLayout()
+        self.main_widget = QWidget()
+        self.layout = QVBoxLayout()
 
         # Create the file name label
-        FILE_NAME_LABEL = QLabel("Aucun fichier chargé")
-        layout.addWidget(FILE_NAME_LABEL)
+        # layout.addWidget(file_name_label)
 
         # Create the figure and CANVAS for the spectrogram
-        FIG, _ = plt.subplots()
-        CANVAS = FigureCanvas(FIG)
-        layout.addWidget(CANVAS)
+        # canvas_width, canvas_height = CANVAS.get_width_height()
 
-        main_widget.setLayout(layout)
-        self.setCentralWidget(main_widget)
+        # Afficher le spectrogramme dans le CANVAS
+        self.fig, _ = plt.subplots()
+        self.canvas = FigureCanvas(self.fig)
+        self.layout.addWidget(self.canvas)
+
+        self.main_widget.setLayout(self.layout)
+        self.setCentralWidget(self.main_widget)
 
         # Create the menu bar
-        menu_bar = self.menuBar()
+        self.menu_bar = self.menuBar()
 
-        file_menu = menu_bar.addMenu("Fichier")
+        self.file_menu = self.menu_bar.addMenu("Fichier")
         load_action = QAction("Charger un fichier audio", self)
-        load_action.triggered.connect(load_audio_file)
-        file_menu.addAction(load_action)
+        load_action.triggered.connect(self.load_audio_file)
+        self.file_menu.addAction(load_action)
 
         save_action = QAction("Enregistrer le spectrogramme", self)
-        save_action.triggered.connect(save_spectrogram)
-        file_menu.addAction(save_action)
+        save_action.triggered.connect(self.save_spectrogram)
+        self.file_menu.addAction(save_action)
 
-        file_menu.addSeparator()
+        self.file_menu.addSeparator()
 
         quit_action = QAction("Quitter", self)
-        quit_action.triggered.connect(on_closing)
-        file_menu.addAction(quit_action)
+        quit_action.triggered.connect(self.on_closing)
+        self.file_menu.addAction(quit_action)
 
-        colormap_menu = menu_bar.addMenu("Colormap")
+        self.colormap_menu = self.menu_bar.addMenu("Colormap")
         for colormap in plt.colormaps():
             colormap_action = QAction(colormap, self)
-            colormap_action.triggered.connect(lambda checked,
-                                        cmap=colormap: update_spectrogram(cmap))
-            colormap_menu.addAction(colormap_action)
+            colormap_action.triggered.connect(lambda checked, self=self,
+                                              cmap=colormap: setattr(self, 'cmap', cmap))
+            colormap_action.triggered.connect(self.update_spectrogram)
+            self.colormap_menu.addAction(colormap_action)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
